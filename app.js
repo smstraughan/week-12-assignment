01f7821db2640910e6c54e9a05ff177ec89af64a
smstraughan <smstraughan@gmail.com>
2026-02-09 17:17:10 -0700

    initiating repository

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+-Create a CRD application (CRUD without update) using json-server or another API
+-Use fetch and async/await to interact with the API
+-Use a form to create/post new entities
+-Build a way for users to delete entities
+-Include a way to get entities from the API and display them
+-You do NOT need update, but you can add it if you'd like
+-Use Bootstrap and/or CSS to style your project
+*/
